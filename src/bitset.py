d270b03e2389741aec7942e13d27683c116fb96c
preyta <preyta@163.com>
2017-06-02 22:24:37 +0800

    mmap based bitset added

---
+++ src/bitset.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import mmap
+
+
+PAGE_SIZE = 4096
+Byte_SIZE = 8
+
+
+class MmapBitSet(object):
+
+    def __init__(self, size):
+        """
+        Initialize a MmapBitSet,
+        :param size: total bit in the set
+        :return:
+        """
+        byte_size = ((size / 8) / PAGE_SIZE + 1) * PAGE_SIZE
+        self._data_store = mmap.mmap(-1, byte_size)
+        self._size = byte_size * 8
+
+    def _write_byte(self, pos, byte):
+        """
+        write byte at pos
+        :param pos: byte position
+        :param byte: byte to write
+        :return: None
+        """
+        self._data_store.seek(pos)
+        self._data_store.write_byte(str(byte))
+
+    def _read_byte(self, pos):
+        """
+        read byte at pos postion
+        :param pos: byte position
+        :return: ascii code of byte that read from datastore
+        """
+        self._data_store.seek(pos)
+        return ord(self._data_store.read_byte())
+
+    def set(self, pos, val=True):
+        """
+        set bit at position "pos" to "val"
+        :param pos: bit position to set
+        :param val: value to set for the bit
+        :return:
+        """
+        assert isinstance(pos, int)
+        if pos < 0 or pos > self._size:
+            raise ValueError('Invalid value bit {bit}, '
+                             'should between {start} - {end}'.format(bit=pos,
+                                                                     start=0,
+                                                                     end=self._size))
+        byte_no = pos / Byte_SIZE
+        inside_byte_no = pos % Byte_SIZE
+
+        raw_byte = self._read_byte(byte_no)
+        if val:
+            set_byte = raw_byte | (2 ** inside_byte_no)
+        else:
+            set_byte = raw_byte & (2**Byte_SIZE - 1 - 2 ** inside_byte_no)
+        self._write_byte(byte_no, set_byte)
+
+    def test(self, pos):
+        """
+        test if a bit is set
+        :param pos:
+        :return: True if specified bit is set, otherwise False
+        """
+        byte_no = pos / Byte_SIZE
+        inside_byte_no = pos % Byte_SIZE
+
+        raw_byte = self._read_byte(byte_no)
+        bit = raw_byte & 2 ** inside_byte_no
+        return True if bit else False
+
+    def __len__(self):
+        return self._size
+
+    def __del__(self):
+        self._data_store.close()
---
+++ test/test_mmap_bit_set.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from src.bitset import MmapBitSet
+
+
+def test_mmap_bit_set_init():
+    bset = MmapBitSet(1000)
+    assert len(bset) == 4096 * 8
+
+    bset = MmapBitSet(10000)
+    assert len(bset) == 4096 * 8
+
+    bset = MmapBitSet(100000)
+    assert len(bset) == 4096 * 8 * 4
+
+
+def test_mmap_bit_test_true():
+    bset = MmapBitSet(1000)
+    bset.set(1)
+    assert bset.test(1) == True
+
+
+def test_mmap_bit_test_false():
+    bset = MmapBitSet(1000)
+    bset.set(1)
+    assert bset.test(2) == False
+
